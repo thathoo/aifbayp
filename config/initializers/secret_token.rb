4e70c36e918dbf2a6c91e1caeeb1b4bcb56c1b4a
Rahul Thathoo <rahul.thathoo@gmail.com>
2012-09-29 15:57:38 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aifbayp::Application.config.secret_token = 'b080bf62f80791a632560d7559db95945553bedab1ccb28aabd05f7b1cab176b69fc43a7893a22f8472796bb5037b0d3a0f9f88067a22d07789df65d9b89a96b'
