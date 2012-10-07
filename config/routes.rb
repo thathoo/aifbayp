2c517da4a132bad7c0db23b9190d7503195f3956
simran <simran@simran-vb.(none)>
2012-10-07 11:02:38 -0700

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Aifbayp::Application.routes.draw do
   get "home/index" 
 
-  root :to => "home#index"
+  #root :to => "home#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
